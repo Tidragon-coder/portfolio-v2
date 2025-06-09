3eabf6af67d19623003b92e802e866e5257fe9bb
Tidragon <theomoon77@gmail.com>
2025-06-09 20:17:23 +0200

    0.5.1 : finition des cards des hard skill

--- src/components/molecules/SkillCard.tsx
+++ src/components/molecules/SkillCard.tsx
@@ -8,10 +8,10 @@ type SkillCardProps = {
 
 const SkillCard = ({ icon, title, description }: SkillCardProps) => {
   return (
-    <div className="flex flex-col items-center bg-primary p-5 rounded-[10px] w-[150px] shadow-md transition-transform duration-300 ease-in-out cursor-pointer hover:scale-110 hover:bg-primary-light group">
+    <div className="flex flex-col items-center bg-primary p-5 rounded-[10px] w-[180px] shadow-md transition-transform duration-300 ease-in-out cursor-pointer hover:scale-110 hover:bg-[#151078] group">
     <div dangerouslySetInnerHTML={{ __html: icon }}  />
     <span className="mt-2 text-lg font-bold text-light-color">{title}</span>
-    <p className="text-sm text-light-color mt-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+    <p className="text-sm text-light-color mt-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 self-center text-center">
         {description}
     </p>
     </div>
--- src/components/molecules/hardSkill.tsx
+++ src/components/molecules/hardSkill.tsx
@@ -6,13 +6,42 @@ import skillsIcons from '../../data/skillsIcons';
 
 const HardSkills = () => {
   return (
-    <section className="flex flex-wrap justify-center gap-6">
+<div className='px-[200px]'>
+  <h2 className="text-3xl font-bold text-center mb-8 mt-[50px]">{'<Hard Skills/>'}</h2>
+  <section className="mb-12">
+      <h3 className="text-2xl font-bold text-center mb-8">Dévéloppement</h3>
+      <div className="flex flex-wrap justify-center gap-6">
         <SkillCard icon={skillsIcons.html} title="HTML" description="Structurer les pages web" />     
         <SkillCard icon={skillsIcons.css} title="CSS" description="Styliser et animer les éléments" />
         <SkillCard icon={skillsIcons.js} title="JavaScript" description="Créer des interactions dynamiques" /> 
         <SkillCard icon={skillsIcons.typescript} title="TypeScript" description="Surcouche de JavaScript avec typage statique" />
         <SkillCard icon={skillsIcons.react} title="React" description="Bibliothèque JS pour créer des interfaces interactives" />
-    </section>
+      </div>
+  </section>
+  <section className="mb-12">
+      <h3 className="text-2xl font-bold text-center mb-8">Back-end & base de données</h3>
+      <div className="flex flex-wrap justify-center gap-6">
+        <SkillCard icon={skillsIcons.java} title="Java" description="Programmation orientée objet" />
+        <SkillCard icon={skillsIcons.php} title="PHP" description="Développement back-end" /> 
+        <SkillCard icon={skillsIcons.laravel} title="Laravel" description="Framework PHP moderne pour créer des applications web robustes" />
+        <SkillCard icon={skillsIcons.mysql} title="SQL / MySQL" description="Gestion de bases de données" /> 
+      </div>
+  </section>
+
+  <section className="mb-12">
+      <h3 className="text-2xl font-bold text-center mb-8">Outils, Workflow & CMS</h3>
+      <div className="flex flex-wrap justify-center gap-6">
+        <SkillCard icon={skillsIcons.github} title="GitHub" description="Versioning et collaboration distant" />     
+        <SkillCard icon={skillsIcons.git} title="Git" description="Versioning et collaboration" />
+        <SkillCard icon={skillsIcons.postman} title="Postman" description="Test des API" /> 
+        <SkillCard icon={skillsIcons.vscode} title="VS Code" description="Environnements de développement" />
+        <SkillCard icon={skillsIcons.intellij} title="IntelliJ" description="Environnements de développement" />
+        <SkillCard icon={skillsIcons.wamp} title="WAMP" description="Serveur local pour PHP & MySQL" />     
+        <SkillCard icon={skillsIcons.figma} title="Figma" description="Conception et prototypage UI/UX" />
+        <SkillCard icon={skillsIcons.wp} title="WordPress" description="CMS et développement de sites Web" /> 
+      </div>
+  </section>
+</div>
   );
 };
 
